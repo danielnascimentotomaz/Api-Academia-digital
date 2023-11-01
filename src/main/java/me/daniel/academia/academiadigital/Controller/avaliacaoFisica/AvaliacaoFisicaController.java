f385b825731e966b15028cd45bda9181b22c67c1
danielnascimentotomaz <danielsabio1000@hotmail.com>
2023-11-01 08:42:06 -0300

    Criação do método getAll para Avaliação Fisica

--- src/main/java/me/daniel/academia/academiadigital/Controller/avaliacaoFisica/AvaliacaoFisicaController.java
+++ src/main/java/me/daniel/academia/academiadigital/Controller/avaliacaoFisica/AvaliacaoFisicaController.java
@@ -7,10 +7,9 @@ import me.daniel.academia.academiadigital.service.avalicaoFisica.AvaliacaoFisica
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/avaliacoes")
@@ -23,7 +22,12 @@ public class AvaliacaoFisicaController {
     public ResponseEntity<AvaliacaoFisica> create(@RequestBody AvaliacaoFisicaDto avaliacaoFisicaDto) throws Exception {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(avaliacaoFisicaService.create(avaliacaoFisicaDto));
+    }
 
+    @GetMapping
+    public ResponseEntity<List<AvaliacaoFisica>> getAll() {
+
+        return ResponseEntity.status(HttpStatus.OK).body(avaliacaoFisicaService.getAll());
     }
 
 }
